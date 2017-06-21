7e9ece274e9df03dd6c66f700dae4e3b12e629b1
Chenglan Guo <chenglanguo@quarkfinance.com>
2017-06-21 19:11:15 +0800

    test

--- prod/dac_tsp.groovy
+++ prod/dac_tsp.groovy
@@ -6,8 +6,6 @@ node {
       stage("CheckoutCode") {
         codeCheckout{
           svnRepo="https://qf-project-01.quark.com:8443/svn/DAC/CodeLib/dac/branches/DAC_MOBILE_20170401"
-          svnCredentialsId="c9baf728-2463-4d59-8643-2181a681fdd4"
-          svnLocal="."
         }
       }
     }
