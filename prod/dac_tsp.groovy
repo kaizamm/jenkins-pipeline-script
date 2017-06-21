ba3737fd417821cea6314227f002051958e19087
Chenglan Guo <chenglanguo@quarkfinance.com>
2017-06-21 19:33:06 +0800

    test

--- prod/dac_tsp.groovy
+++ prod/dac_tsp.groovy
@@ -3,10 +3,16 @@
 node {
   stage('Docker Build') {
     docker.image('172.30.33.31:5000/service/maven:3.5.0-8u74').inside("--add-host qf-javadev-01:172.16.1.39 -v /data/maven_repo:/home/qkuser/.m2") {
-      stage("CheckoutCode") {
+      stage("检出源码") {
         codeCheckout{
           svnRepo="https://qf-project-01.quark.com:8443/svn/DAC/CodeLib/dac/branches/DAC_MOBILE_20170401"
         }
+      }
+      stage("执行测试") {
+        mvnTest()
+      }
+      stage("执行构建") {
+        mvnPackage()
       }
     }
   }
