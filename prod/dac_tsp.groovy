4cfab9697eeb747ae1fb67b0abe4c9955a6cb7ac
Chenglan Guo <chenglanguo@quarkfinance.com>
2017-06-21 18:56:49 +0800

    test

--- prod/dac_tsp.groovy
+++ prod/dac_tsp.groovy
@@ -1,26 +1,12 @@
 #!/usr/bin/env groovy
 @Library('shareMaven') _
-pipeline {
-  agent none
-  stages {
-    stage('Build') {
-      agent {
-        docker {
-          image '172.30.33.31:5000/service/maven:3.5.0-8u74'
-          args '--add-host qf-javadev-01:172.16.1.39 -v /data/maven_repo:/home/qkuser/.m2'
+node {
+  stage('Docker Build') {
+    docker.image('172.30.33.31:5000/service/maven:3.5.0-8u74').inside {
+      stage("CheckoutCode") {
+        CheckoutCode{
+          svnRepo="https://qf-project-01.quark.com:8443/svn/DAC/CodeLib/dac/branches/DAC_MOBILE_20170401"
         }
-      }
-        codeCheckout{
-          svnRepo='https://qf-project-01.quark.com:8443/svn/DAC/CodeLib/dac/branches/DAC_MOBILE_20170401'
-        }
-    }
-
-    stage('Generate Dockerfile') {
-      agent {
-        label 'jenkins-slave-01'
-      }
-      steps {
-        echo "ok"
       }
     }
   }
