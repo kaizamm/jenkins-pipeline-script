ae36edb20cd18596f0c3fa887a070b5917b2ccdc
Chenglan Guo <chenglanguo@quarkfinance.com>
2017-06-23 15:44:25 +0800

    finish

--- prod/dac_tsp.groovy
+++ prod/dac_tsp.groovy
@@ -19,45 +19,51 @@ node {
   }
   withEnv(envList) {
     stage ('选择动作') {
-    def actionInput = input (
-      id: 'actionInput', message: 'Choice your action!', parameters: [
-      [$class: 'ChoiceParameterDefinition', choices: "deploy\nrollback", description: 'choice your action!', name: 'action']
-      ])
-    def action = actionInput.trim()
-    if (action == 'deploy') {
-        docker.image("${env.dockerMavenImage}").inside("${env.dockerMavenOpt}") {
-          stage("检出源码") {
-            codeCheckout{
-              svnRepo="${this.env.svnRepo}"
-              // svnCredentialsId="${this.env.svnCredentialsId}"
-              // svnLocal="${this.env.svnLocal}"
+      def actionInput = input (
+        id: 'actionInput', message: 'Choice your action!', parameters: [
+        [$class: 'ChoiceParameterDefinition', choices: "deploy\nrollback", description: 'choice your action!', name: 'action']
+        ])
+        def action = actionInput.trim()
+        if (action == 'deploy') {
+          // 在docker内部代码检出、执行测试、执行包构建
+          docker.image("${env.dockerMavenImage}").inside("${env.dockerMavenOpt}") {
+            stage("检出源码") {
+              codeCheckout{
+                svnRepo="${this.env.svnRepo}"
+                // svnCredentialsId="${this.env.svnCredentialsId}"
+                // svnLocal="${this.env.svnLocal}"
+              }
+            }
+            stage("执行测试") {
+              mvnTest()
+            }
+            stage("包构建") {
+              mvnPackage()
             }
           }
-          stage("执行测试") {
-            mvnTest()
+
+          // docker 镜像构建
+          stage('镜像构建') {
+            dockerBuild {
+              propertiesPath = '/data/prepare_dac_tsp.properties'
+            }
           }
-          stage("包构建") {
-            mvnPackage()
+
+          // 部署操作
+          stage('部署生产') {
+            deployContainer {
+              propertiesPath = '/data/prepare_dac_tsp.properties'
+            }
           }
-        }
-      stage('镜像构建') {
-        dockerBuild {
-          propertiesPath = '/data/prepare_dac_tsp.properties'
-        }
-      }
-      stage('部署生产') {
-        deployContainer {
-          propertiesPath = '/data/prepare_dac_tsp.properties'
-        }
-      }
-    } else {
-      stage('版本回滚') {
-        rollbackContainer {
-          propertiesPath = '/data/prepare_dac_tsp.properties'
-          getRegistryTagList= '/data/jenkins_etcd/getRegistryTagList.py'
+        } else {
+          // 版本回滚操作，针对镜像的版本回滚，会调用共享库类的几个stage操作
+          stage('版本回滚') {
+            rollbackContainer {
+              propertiesPath = '/data/prepare_dac_tsp.properties'
+              getRegistryTagList= '/data/jenkins_etcd/getRegistryTagList.py'
+            }
+          }
         }
       }
     }
   }
-}
-}
