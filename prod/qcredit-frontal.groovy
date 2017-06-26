55a35cc7a2f40a26d76411a1171c66235c3c54c3
Chenglan Guo <chenglanguo@quarkfinance.com>
2017-06-26 18:27:46 +0800

    add qcredit.groovy

--- prod/qcredit-frontal.groovy
+++ prod/qcredit-frontal.groovy
@@ -14,11 +14,43 @@ node {
           // 在docker内部代码检出、执行测试、执行包构建
           docker.image("${env.dockerMavenImage}").inside("${env.dockerMavenRunOpts}") {
             stage("检出源码") {
+              // 检出base源码
               codeCheckout{
-                svnRepo="${this.env.svnRepo}"
+                svnRepo="${this.env.qfBaseRepo}"
                 // svnCredentialsId="${this.env.svnCredentialsId}"
-                // svnLocal="${this.env.svnLocal}"
+                svnLocal="${this.env.qfBaseLocal}"
               }
+              // 检出parent
+              codeCheckout {
+                svnRepo="${this.env.qfParentRepo}"
+                svnLocal="${this.env.qfParentLocal}"
+              }
+              // 检出qf-redis
+              codeCheckout {
+                svnRepo="${this.env.qfRedisRepo}"
+                svnLocal="${this.env.qfRedisLocal}"
+              }
+              // 检出qf-core
+              codeCheckout {
+                svnRepo="${this.env.qfCoreRepo}"
+                svnLocal="${this.env.qfCoreLocal}"
+              }
+              // 检出qf-entities
+              codeCheckout {
+                svnRepo="${this.env.qfEntitiesRepo}"
+                svnLocal="${this.env.qfEntitiesLocal}"
+              }
+              // 检出qcredit-bkend
+              codeCheckout {
+                svnRepo="${this.env.qcreditBkendRepo}"
+                svnLocal="${this.env.qcreditBkendLocal}"
+              }
+              // 检出qcredit-frontal
+              codeCheckout {
+                svnRepo="${this.env.qcreditFrontalRepo}"
+                svnLocal="${this.env.qcreditFrontalLocal}"
+              }
+
             }
             stage("执行测试") {
               mvnTest()
