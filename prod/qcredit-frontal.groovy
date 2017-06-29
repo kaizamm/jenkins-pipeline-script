d481bd82f2eb4dc9fe8a585b9028837d87c8b6bb
Chenglan Guo <chenglanguo@quarkfinance.com>
2017-06-29 13:45:29 +0800

    qcredit jenkinspipelie

--- prod/qcredit-frontal.groovy
+++ prod/qcredit-frontal.groovy
@@ -3,94 +3,83 @@
 
 node {
   // 读取properties文件
-  def envList = myLoadProperties "/data/jenkinspipeline-qcredit.properties"
-  withEnv(envList) {
-    stage ('选择动作') {
-      try {
-        // action 选择，有deploy和rollback两种动作
-        def action = choiceAction ()
-
-        if (action == 'deploy') {
-          // 在docker内部代码检出、执行测试、执行包构建
-          docker.image("${env.dockerMavenImage}").inside("${env.dockerMavenRunOpts}") {
-            stage("检出源码") {
-              // 检出base源码
-              codeCheckout{
-                svnRepo="${this.env.qfBaseRepo}"
-                // svnCredentialsId="${this.env.svnCredentialsId}"
-                svnLocal="${this.env.qfBaseLocal}"
-              }
-              // 检出parent
-              codeCheckout {
-                svnRepo="${this.env.qfParentRepo}"
-                svnLocal="${this.env.qfParentLocal}"
-              }
-              // 检出qf-redis
-              codeCheckout {
-                svnRepo="${this.env.qfRedisRepo}"
-                svnLocal="${this.env.qfRedisLocal}"
-              }
-              // 检出qf-core
-              codeCheckout {
-                svnRepo="${this.env.qfCoreRepo}"
-                svnLocal="${this.env.qfCoreLocal}"
-              }
-              // 检出qf-entities
-              codeCheckout {
-                svnRepo="${this.env.qfEntitiesRepo}"
-                svnLocal="${this.env.qfEntitiesLocal}"
-              }
-              // 检出qcredit-bkend
-              codeCheckout {
-                svnRepo="${this.env.qcreditBkendRepo}"
-                svnLocal="${this.env.qcreditBkendLocal}"
-              }
-              // 检出qcredit-frontal
-              codeCheckout {
-                svnRepo="${this.env.qcreditFrontalRepo}"
-                svnLocal="${this.env.qcreditFrontalLocal}"
-              }
-
+  myLoadProperties ('172.30.33.31',2379,"/quarkfinance.com/instances/${env.JOB_BASE_NAME}/jenkinspipeline.properties")
+  stage ('选择动作') {
+    try {
+      // action 选择，有deploy和rollback两种动作
+      def action = choiceAction ()
+      if (action == 'deploy') {
+        // 在docker内部代码检出、执行测试、执行包构建
+        docker.image("${env.dockerMavenImage}").inside("${env.dockerMavenRunOpts}") {
+          stage("检出源码") {
+            // 检出base源码
+            codeCheckout{
+              svnRepo="${this.env.qfBaseRepo}"
+              // svnCredentialsId="${this.env.svnCredentialsId}"
+              svnLocal="${this.env.qfBaseLocal}"
             }
-            stage("执行测试") {
-              // 如果代码是取到当前"."目录下，则直接用下面即可
-              // mvnTest()
-              // 否则使用properties里面的内容
-              // mvnTest("${this.env.mavenTestOpts}")
-              mvnTest("${this.env.mavenTestOpts}")
+            // 检出parent
+            codeCheckout {
+              svnRepo="${this.env.qfParentRepo}"
+              svnLocal="${this.env.qfParentLocal}"
             }
-            stage("包构建") {
-              mvnPackage("${this.env.mavenPackageOpts}")
+            // 检出qf-redis
+            codeCheckout {
+              svnRepo="${this.env.qfRedisRepo}"
+              svnLocal="${this.env.qfRedisLocal}"
+            }
+            // 检出qf-core
+            codeCheckout {
+              svnRepo="${this.env.qfCoreRepo}"
+              svnLocal="${this.env.qfCoreLocal}"
+            }
+            // 检出qf-entities
+            codeCheckout {
+              svnRepo="${this.env.qfEntitiesRepo}"
+              svnLocal="${this.env.qfEntitiesLocal}"
+            }
+            // 检出qcredit-bkend
+            codeCheckout {
+              svnRepo="${this.env.qcreditBkendRepo}"
+              svnLocal="${this.env.qcreditBkendLocal}"
+            }
+            // 检出qcredit-frontal
+            codeCheckout {
+              svnRepo="${this.env.qcreditFrontalRepo}"
+              svnLocal="${this.env.qcreditFrontalLocal}"
             }
           }
-
-          // docker 镜像构建
-          stage('镜像构建') {
-            dockerBuild {
-              propertiesPath = '/data/jenkinspipeline-qcredit.properties'
-            }
+          stage("执行测试") {
+            // 如果代码是取到当前"."目录下，则直接用下面即可
+            // mvnTest()
+            // 否则使用properties里面的内容
+            mvnTest("${this.env.mavenTestOpts}")
+            // mvnTest()
           }
-
-          // 部署操作
-          stage('部署生产') {
-            deployContainer {
-              propertiesPath = '/data/jenkinspipeline-qcredit.properties'
-            }
-          }
-        } else {
-          // 版本回滚操作，针对镜像的版本回滚，会调用共享库类的几个stage操作
-          stage('版本回滚') {
-            rollbackContainer {
-              propertiesPath = '/data/jenkinspipeline-qcredit.properties'
-              getRegistryTagList= '/data/jenkins_etcd/getRegistryTagList.py'
-            }
+          stage("包构建") {
+            mvnPackage("${this.env.mavenPackageOpts}")
           }
         }
-      } catch (exc) {
-        sendEmail {
-          emailRecipients= "${this.env.projectRecipientList}"
-          error exc
+        // docker 镜像构建
+        stage('镜像构建') {
+          dockerBuild {}
         }
+        // 部署操作
+        stage('部署生产') {
+          deployContainer {}
+        }
+      } else {
+        // 版本回滚操作，针对镜像的版本回滚，会调用共享库类的几个stage操作
+        stage('版本回滚') {
+          rollbackContainer {
+            getRegistryTagList= '/data/jenkins_etcd/getRegistryTagList.py'
+          }
+        }
+      }
+    } catch (exc) {
+      sendEmail {
+        emailRecipients= "${this.env.projectRecipientList}"
+        error exc
       }
     }
   }
