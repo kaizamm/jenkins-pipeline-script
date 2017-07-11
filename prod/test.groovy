2fd08c5b241325bf95105f498d0099e1ca94f6d1
Chenglan Guo <chenglanguo@quarkfinance.com>
2017-07-11 11:39:44 +0800

    test

--- prod/test.groovy
+++ prod/test.groovy
@@ -3,7 +3,7 @@
 node {
   stage('检出源码'){
     gitCodeCheckout{
-      gitRepo='ssh://git@172.16.4.213:222/OPS/jenkins-pipeline-script.git'
+      gitRepo='http://172.16.4.213/OPS/jenkins_etcd.git'
       gitLocal='jenkins_etcd'
       gitCredentialsId='f7f592e0-8748-4c06-ac5d-14c1c98f46b4'
     }
