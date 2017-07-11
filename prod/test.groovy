af5fe951a8551f533afccabb02e22e4f2ef9c3af
Chenglan Guo <chenglanguo@quarkfinance.com>
2017-07-11 11:37:37 +0800

    change gitRepo for ssh

--- prod/test.groovy
+++ prod/test.groovy
@@ -3,7 +3,7 @@
 node {
   stage('检出源码'){
     gitCodeCheckout{
-      gitRepo='http://172.16.4.213/OPS/jenkins_etcd.git'
+      gitRepo='ssh://git@172.16.4.213:222/OPS/jenkins-pipeline-script.git'
       gitLocal='jenkins_etcd'
       gitCredentialsId='f7f592e0-8748-4c06-ac5d-14c1c98f46b4'
     }
