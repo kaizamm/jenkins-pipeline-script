861f4e8f3eab17230c29a24eab8b1ba6f9ffc2fb
Jiashun Chen <jiashunc@quarkfinance.com>
2017-07-13 08:33:49 +0000

    Update nxd-esb-bizapp.groovy

--- prod/nxd-esb-bizapp.groovy
+++ prod/nxd-esb-bizapp.groovy
@@ -19,13 +19,13 @@ node {
               gitLocal="${this.env.gitLocal}"
             }
           }
-          stage("执行测试") {
+          //stage("执行测试") {
             // 如果代码是取到当前"."目录下，则直接用下面即可
             // mvnTest()
             // 否则使用properties里面的内容
             // mvnTest("${this.env.mavenTestOpts}")
-            mvnTest()
-          }
+            //mvnTest()
+          //}
           stage("包构建") {
             mvnPackage("${this.env.mavenPackageOpts}")
           }
